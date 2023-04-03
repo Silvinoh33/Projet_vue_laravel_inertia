054f6bbb62436e44be185d61bd8083c44cade369
Silvinoh <edhorgounon@gmail.com>
2023-04-03 19:48:58 +0200

    original

---
+++ resources/js/Pages/Footer.vue
@@ -0,0 +1,32 @@
+<template >
+     <div class="flex">
+      <p>
+        <a href="https://www.linkedin.com/in/cher-telleconseiller/" target="_blank" rel="noopener noreferrer">
+          <img src="https://img.icons8.com/ios-filled/50/000000/linkedin.png" alt="LinkedIn" />
+        </a>
+      </p>
+      <p>
+        <a href="https://github.com/cher-telleconseiller" target="_blank" rel="noopener noreferrer">
+          <img src="https://img.icons8.com/ios-filled/50/000000/github.png" alt="GitHub" />
+        </a>
+      </p>
+      <p>
+        <a href="https://twitter.com/cher_telleconseiller" target="_blank" rel="noopener noreferrer">
+          <img src="https://img.icons8.com/ios-filled/50/000000/twitter.png" alt="Twitter" />
+        </a>
+      </p>
+      <p>
+        <a href="https://www.instagram.com/cher_telleconseiller/" target="_blank" rel="noopener noreferrer">
+          <img src="https://img.icons8.com/ios-filled/50/000000/instagram.png" alt="Instagram" />
+        </a>
+      </p>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
